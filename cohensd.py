aa8d0281a88a9bbb5fa44164e21d1c4681df5834
Siddharth Mangalik <smangalik@cs.stonybrook.edu>
2021-12-07 18:56:48 -0500

    Cohen's D on higher aggregations

--- cohensd.py
+++ cohensd.py
@@ -4,12 +4,14 @@ from pymysql import cursors, connect
 import pandas as pd
 import numpy as np
 import random, sys
+import scipy.stats as ss
+from utils import yearweek_to_dates, date_to_quarter
 
 d_threshold = 0.1
 
 def permutation_cohens_d(x, n_perms=30):
-    if len(x) <= 2: return 0.0, np.std(x)/np.sqrt(len(x))
-    if np.std(x) == 0: return 0.0, 0.0
+    if len(x) <= 2: return np.nan, np.nan
+    if np.std(x) < 1e-6: return np.nan, np.nan 
     ds = []
     std = np.std(x)
     random.seed(a=25, version=2)
@@ -24,8 +26,8 @@ def permutation_cohens_d(x, n_perms=30):
     return avg_d, d_stderr
 
 def cohens_d(x):
-    if len(x) <= 2: return 0.0
-    if np.std(x) == 0: return 0.0
+    if len(x) <= 2: return np.nan
+    if np.std(x) < 1e-6: return np.nan, np.nan 
     random.seed(a=25, version=2)
     shuffled = random.sample(x, len(x))
     split_index = len(shuffled)//2
@@ -36,9 +38,22 @@ def cohens_d(x):
 
 def main():
 
+    # experimenting with n
+    # for n in range(3,100,5):
+    #     x = ss.bernoulli.rvs(size=n, p=0.5)
+    #     d, _ = permutation_cohens_d(list(x))
+    #     print(x,n,d,'\n')
+    # sys.exit()
+
     # Open default connection
     print('Connecting to MySQL...')
     connection  = connect(read_default_file="~/.my.cnf")
+
+    # Get supplemental data
+    county_info = pd.read_csv("county_fips_data.csv",encoding = "utf-8")
+    county_info['cnty'] = county_info['fips'].astype(str).str.zfill(5)
+    msa_info = pd.read_csv("county_msa_mapping.csv")
+    msa_info['cnty'] = msa_info['fips'].astype(str).str.zfill(5)
 
     # NOT INDIVIDUAL RESULTS
     # tables = ["ctlb2.feat$dd_depAnxLex$timelines2019$yw_cnty$1gra",
@@ -53,7 +68,7 @@ def main():
     # Gallup COVID Panel
     tables = ["gallup_covid_panel_micro_poll.old_hasSadBefAug17_recodedEmoRaceGenPartyAge_v3_02_15"]
     feat_val_col = "WEC_sadF" # WEB_worryF, WEC_sadF, pos_affect, neg_affect
-    groupby_col = "yearweek_cnty" # cnty, yearweek, yearweek_cnty
+    groupby_col = "quarter_division" # cnty, yearweek, yearweek_cnty, division_name, yearweek_msa, month_msa, month_state, quarter_state, quarter_division
     feat_value = feat_val_col
     filter = "WHERE {} IS NOT NULL".format(feat_val_col) 
     relevant_columns = "fips, yearweek, WEA_enjoyF, WEB_worryF, WEC_sadF, WEI_depressionF, WEJ_anxietyF, pos_affect, neg_affect"
@@ -94,16 +109,28 @@ def main():
         df = df.rename(columns={"fips":"cnty"})
         df['yearweek'] = df['yearweek'].str[:4] + "_" + df['yearweek'].str[4:]
         df['yearweek_cnty'] = df['yearweek'] + ":" + df['cnty']
+        df['date'] = df['yearweek'].apply(lambda x: yearweek_to_dates(x)[1])
+        df['month'] = df['date'].apply(lambda x: str(x.month))
+        df['quarter'] = df['date'].apply(lambda x: date_to_quarter(x))
+        df = pd.merge(df,county_info[['cnty','state_name','region_name','division_name']],on='cnty')
+        df['month_state'] = df['month'] + ":" + df['state_name']
+        df['quarter_state'] = df['quarter'] + ":" + df['state_name']
+        df['quarter_division'] = df['quarter'] + ":" + df['division_name']
+        if 'msa' in groupby_col:
+            df = pd.merge(df,msa_info[['cnty','msa']],on='cnty')
+            df = df[df['msa'].notna()]
+            df['yearweek_msa'] = df['yearweek'] + ":" + df['msa']
+            df['month_msa'] = df['month'] + ":" + df['msa']
         
-    
-    print(df.head(25))
+    # Peek the clearned data
+    print(df)
 
     grouped = df.groupby(groupby_col)[feat_val_col].apply(list).reset_index(name=feat_value)
     grouped[feat_value] = grouped[feat_value].apply(lambda x: [i for i in x if str(i) != "nan"]) # clean NaNs
     grouped['std'] = [np.array(x).std() for x in grouped[feat_value].values]
     
 
-    min_len = 0
+    min_len = 2
     grouped['n'] = [len(x) for x in grouped[feat_value].values]  
     grouped = grouped[grouped['n'] >= min_len] # list has more than 2 values
 
@@ -117,7 +144,6 @@ def main():
     
 
     # Filter D Values
-    #significant = grouped[grouped['d_perm'] < d_threshold]
     significant_mask = grouped['d_perm+ci'] < d_threshold
     significant = grouped[significant_mask]
     insignificant = grouped[~significant_mask]
@@ -125,7 +151,7 @@ def main():
 
     print("\nFor {} and {} we find the following {}/{} groups to have a Permutation Cohen's D less than {}:\n".format(
         feat_value, groupby_col, len(significant), len(grouped), d_threshold))
-    print("Median / Mean n = {} / {}".format( np.median(grouped['n']), round(np.mean(grouped['n'])), 2) )
+    print("Overall Median / Mean n = {} / {}".format( np.median(grouped['n']), round(np.mean(grouped['n'])), 2) )
     print("Average Perm D  = {}; Average Perm D + CI = {}".format( round(np.mean(grouped['d_perm']),4), round(np.mean(grouped['d_perm+ci']),4) )) 
     print("Std Dev Perm D = {}; Std Dev Perm D + CI = {}".format( np.std(grouped['d_perm']), np.std(grouped['d_perm+ci']) )) 
     print()
---
+++ utils.py
@@ -0,0 +1,20 @@
+# Returns the first and last day of a week given as "2020_11"
+import datetime
+
+def date_to_yearweek(d):
+  year, weeknumber, weekday = d.date().isocalendar()
+  return str(year) + "_" + str(weeknumber)
+
+def yearweek_to_dates(yw):
+  year, week = yw.split("_")
+  year, week = int(year), int(week)
+
+  first = datetime.datetime(year, 1, 1)
+  base = 1 if first.isocalendar()[1] == 1 else 8
+  monday = first + datetime.timedelta(days=base - first.isocalendar()[2] + 7 * (week - 1))
+  sunday = monday + datetime.timedelta(days=6)
+  thursday = monday + datetime.timedelta(days=3)
+  return monday, thursday, sunday
+
+def date_to_quarter(dt):
+    return "Q{}".format( (dt.month+2) // 3 )
